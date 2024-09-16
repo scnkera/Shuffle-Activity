17ff61ae5eb5843c9fa55fe03a06cc3a933e73ec
Christelle Nkera <scnkera@gmail.com>
2024-09-16 19:39:47 -0400

    Updated python file

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 CARDS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 SUITS = ["♥", "♦", "♠", "♦︎"]
 
@@ -6,4 +8,16 @@ deck_of_cards = [f"{card}{suit}" for card in CARDS for suit in SUITS]
 # Implement a solution for shuffling a deck of cards without using random.shuffle()
 # Using randint() is ok
 def shuffle_deck(deck_of_cards):
-    pass
+    card_list = []
+
+    for i in range(52):
+        card_index = random.randint(0, len(deck_of_cards)-1)
+        deck_of_cards.pop(card_index)
+        card_list.append(deck_of_cards[card_index])
+
+    print(card_list)
+
+shuffle_deck(deck_of_cards)
+# shuffle_deck(deck_of_cards)
+# shuffle_deck(deck_of_cards)
+# shuffle_deck(deck_of_cards)
